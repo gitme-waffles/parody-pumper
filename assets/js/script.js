285b39003449e1b5ef4f740af41e63c0d53a930c
leena <annachandy123@gmail.com>
2021-09-29 21:42:41 +1000

    lyrics updated in p tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 var form = $(".form");
 var searchBtn = $("#search");
 var resultArea = $(".result-container");
-var inputSong = $("#input");
+var inputSong = $("#songName");
+var inputArtist = $("#artistName");
 searchBtn.on("click", enterSong);
 function enterSong(event) {
     event.preventDefault();
     var songtoSearch = inputSong.val().trim();
+    var artisttoSearch = inputArtist.val().trim();
     if (songtoSearch) {
         searchSong("ACDC", "Highway to hell");
+        // searchSong(songtoSearch, artisttoSearch);
     }
 }
 
@@ -26,24 +29,31 @@ function searchSong(artist, song) {
             console.log(data);
             var lyrics = data.lyrics.split("\r\n")[1];
             var individualLyric = lyrics.split("\n");
+            getLyrics(lyrics, individualLyric);
         })
         .catch(function (error) {
             console.error(error);
         });
 }
-                     
-function convertText(){ 
-    $('.lyric-api-text').each(function() {
+function getLyrics(lyrics, individualLyric) {
+    $("form")[0].reset();
+    $(".lyric-text").text = " ";
+    console.log(lyrics, "lyrics of song");
+    console.log(individualLyric, "individual Lyrics");
+    $(".lyric-text").append(lyrics);
+}
+
+function convertText() {
+    $(".lyric-api-text").each(function () {
         var lyricEl = $(this);
-        var x = lyricEl.text().replace(/(\w+)/gi, "<span>$1</span>")
-        $('.lyric-api-text').text(''); // clear the text
-        $('.lyric-api-text').append(x); // add the clickable text
+        var x = lyricEl.text().replace(/(\w+)/gi, "<span>$1</span>");
+        $(".lyric-api-text").text(""); // clear the text
+        $(".lyric-api-text").append(x); // add the clickable text
     });
 }
 
-
-$('.lyric-api-text').on('click', 'span', function(event){
-    console.log($(event.target).text())
+$(".lyric-api-text").on("click", "span", function (event) {
+    console.log($(event.target).text());
     // click on text function goes here
 });
 
