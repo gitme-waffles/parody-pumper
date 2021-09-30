8e1afa722a566f4c92b4a4151176c0f8cd055850
Stuart Simmons <stuart@Stuart.local>
2021-09-30 19:29:12 +1000

    changed lyric API

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,73 +3,112 @@ var searchBtn = $("#search");
 var resultArea = $(".result-container");
 var inputSong = $("#songName");
 var inputArtist = $("#artistName");
+var $lyricText = $(".lyric-text");
+
 searchBtn.on("click", enterSong);
 function enterSong(event) {
-    event.preventDefault();
-    var songtoSearch = inputSong.val().trim();
-    var artisttoSearch = inputArtist.val().trim();
-    if (songtoSearch && artisttoSearch) {
-        //  searchSong("ACDC", "Highway to hell");
-        searchSong(songtoSearch, artisttoSearch);
-        addHeading(songtoSearch, artisttoSearch);
-    }
+  event.preventDefault();
+  var songtoSearch = inputSong.val().trim();
+  var artisttoSearch = inputArtist.val().trim();
+  if (songtoSearch && artisttoSearch) {
+    //  searchSong("ACDC", "Highway to hell");
+    searchSong(songtoSearch, artisttoSearch);
+    addHeading(songtoSearch, artisttoSearch);
+  }
 }
 
 function searchSong(song, artist) {
-    var songUrl =
-        "https://private-anon-e83b93ca5b-lyricsovh.apiary-proxy.com/v1/" +
-        encodeURIComponent(artist) +
-        "/" +
-        encodeURIComponent(song);
+  var songUrl =
+    "https://private-anon-e83b93ca5b-lyricsovh.apiary-proxy.com/v1/" +
+    encodeURIComponent(artist) +
+    "/" +
+    encodeURIComponent(song);
 
-    fetch(songUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data, "data");
+  // DO NOT REMOVE!
+  // fetch(songUrl)
+  //     .then(function (response) {
+  //         console.log(response);
+  //         return response.json();
+  //     })
+  //     .then(function (data) {
+  //         console.log(data, "data");
 
-            var lyrics = data.lyrics.split("\r\n")[1];
-            var individualLyric = lyrics.split("\n");
-            getLyrics(lyrics, individualLyric);
-            convertText();
-        })
-        .catch(function (error) {
-            console.error(error);
-        });
+  //         var lyrics = data.lyrics.split("\r\n")[1];
+  //         var individualLyric = lyrics.split("\n");
+  //         getLyrics(lyrics, individualLyric);
+  //         convertText();
+  //     })
+  //     .catch(function (error) {
+  //         console.log(error);
+  //     });
+
+  var mockedResponse = new Promise(function (res) {
+    res({
+      lyrics:
+        "Paroles de la chanson Adventure Of A Lifetime par Coldplay\r\nTurn your magic on\nTo me she'd say\nEverything you want's a dream away\nWe are legends\nEvery day\nThat's what she told him!\n\nTurn your magic on\nTo me she'd say\nEverything you want's a dream away\nUnder this pressure, under this weight\nWe are diamonds\n\nI feel my heart beating\nI feel my heart beneath my skin\nI feel my heart beating\n\n\nOhhh, you make me feel\nLike I'm alive again\nAlive again\nOhhh, you make me feel\nLike I'm alive again\n\nSaid I can't go on, not in this way\nI'm a dream, I die by light of day\nGonna hold up half the sky and say\nOhhh, we are omen\n\nI feel my heart beating\nI feel my heart beneath my skin\nOhhh, I can feel my heart beating\nCause you make me feel\nLike I'm alive again\nAlive again...\n\nOhhh, you make me feel\nLike I'm alive again\n\nTurn your magic on, to me she'd say\nEverything you want's a dream away\nUnder this pressure, under this weight\n\nWe are diamonds taking shape!\nWe are diamonds taking shape!\n\nIf we've only got this life\nThen this adventure, more than I\nAnd if we've only got this life\nYou'll get me through alive\nAnd if we've only got this life\nThen this adventure, more than I\nWanna share with you\nWith you, with you\n\nI said, oh, say oh\n\nWoo hoo, woo hoo...",
+    });
+  });
+
+  mockedResponse.then(function (data) {
+    console.log(data, "data");
+
+    var lyrics = data.lyrics.split("\r\n")[1];
+    console.log(lyrics);
+    renderLyricsToScreen(lyrics);
+    convertText();
+  });
 }
 
 //function to display song in container
-function getLyrics(lyrics, individualLyric) {
-    $("form")[0].reset();
-    $(".lyric-text").text(" ");
+function renderLyricsToScreen(lyrics) {
+  inputArtist.val("");
+  inputSong.val("");
 
-    console.log(lyrics, "lyrics of song");
-    // console.log(individualLyric, "individual Lyrics");
+  console.log(lyrics, "lyrics of song");
+  // console.log(individualLyric, "individual Lyrics");
+  var splitLyricsIntoLines = lyrics.split("\n");
 
-    $(".lyric-text").append(lyrics);
+  for (var i = 0; i < splitLyricsIntoLines.length; i++) {
+    // "" // we add a br
+    // "Text" // we wrap it in a div
+    if (lyrics[i] !== "") {
+      var paraContainer = $("<div>");
+      var words = splitLyricsIntoLines[i].split(" ");
+      for (let j = 0; j < words.length; j++) {
+        var span = $("<span>");
+        span.text(words[j]);
+
+        span.attr("word", words[j]);
+        paraContainer.append(span);
+        $lyricText.append(paraContainer);
+      }
+    } else {
+      var breakElem = $("<br>");
+      $lyricText.append(breakElem);
+    }
+  }
+
+  $(".lyric-text").append(lyrics);
 }
 
 //function to dispaly song name and artist name as heading
 function addHeading(song, artist) {
-    $("#song").text(" ");
-    $("#artist").text(" ");
-    $("#song").append(song.toUpperCase());
-    $("#artist").append(artist.toUpperCase());
+  $("#song").text(" ");
+  $("#artist").text(" ");
+  $("#song").append(song.toUpperCase());
+  $("#artist").append(artist.toUpperCase());
 }
 
 function convertText() {
-    $(".lyric-text").each(function () {
-        var lyricEl = $(this);
-        var x = lyricEl.text().replace(/(\w+)/gi, "<span>$1</span>");
-        $(".lyric-text").text(""); // clear the text
-        $(".lyric-text").append(x); // add the clickable text
-    });
+  //   $(".lyric-text").each(function () {
+  //     var lyricEl = $(this);
+  //     var x = lyricEl.text().replace(/(\w+)/gi, "<span>$1</span>");
+  //     $(".lyric-text").text(""); // clear the text
+  //     $(".lyric-text").append(x); // add the clickable text
+  //   });
 }
 
 $(".lyric-text").on("click", "span", function (event) {
-    console.log($(event.target).text());
-    // click on text function goes here
+  console.log($(event.target).text());
+  // click on text function goes here
 });
-
-
