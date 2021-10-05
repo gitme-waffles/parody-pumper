6c3236a8c1526f47e4b448fd3db98d7efb5dfb1e
leena <annachandy123@gmail.com>
2021-10-05 21:17:30 +1100

    copy modal fixed

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,7 +17,6 @@ var toggleCngTxt = false;
 
 searchBtn.on("click", enterSong);
 $copyToclip.on("click", copyFunc);
-
 function enterSong(event) {
     event.preventDefault();
     songtoSearch = inputSong.val().trim();
@@ -73,74 +72,81 @@ function renderModal() {
 }
 
 function searchSong(song, artist) {
-  var songUrl =
-    "https://private-anon-e83b93ca5b-lyricsovh.apiary-proxy.com/v1/" +
-    encodeURIComponent(artist) +
-    "/" +
-    encodeURIComponent(song);
+    var songUrl =
+        "https://private-anon-e83b93ca5b-lyricsovh.apiary-proxy.com/v1/" +
+        encodeURIComponent(artist) +
+        "/" +
+        encodeURIComponent(song);
 
-  // DO NOT REMOVE!
-  // fetch(songUrl)
-  //   .then(function (response) {
-  //       console.log(response);
-  //       if (response.status != 200) {
-  //         modalError(response.status)
-  //       }
-  //       return response.json();
-  //   })
-  //   .then(function (data) {
-  //     console.log(data, "data");
+    // DO NOT REMOVE!
+    // fetch(songUrl)
+    //   .then(function (response) {
+    //       console.log(response);
+    //       if (response.status != 200) {
+    //         modalError(response.status)
+    //       }
+    //       return response.json();
+    //   })
+    //   .then(function (data) {
+    //     console.log(data, "data");
 
-  //     var lyrics = data.lyrics.split("\r\n")[1];
-  //     //console.log(lyrics);
-  //     renderLyricsToScreen(lyrics);
-  //   }).catch(function(e) {
-  //     console.log(e);
-  //     // modalError(e.message);
-  // });
+    //     var lyrics = data.lyrics.split("\r\n")[1];
+    //     //console.log(lyrics);
+    //     renderLyricsToScreen(lyrics);
+    //   }).catch(function(e) {
+    //     console.log(e);
+    //     // modalError(e.message);
+    // });
 
-  var mockedResponse = new Promise(function (res) {
-    res({
-      lyrics:
-        "Paroles de la chanson Adventure Of A Lifetime par Coldplay\r\nTurn your magic on\nTo me she'd say\nEverything you want's a dream away\nWe are legends\nEvery day\nThat's what she told him!\n\nTurn your magic on\nTo me she'd say\nEverything you want's a dream away\nUnder this pressure, under this weight\nWe are diamonds\n\nI feel my heart beating\nI feel my heart beneath my skin\nI feel my heart beating\n\n\nOhhh, you make me feel\nLike I'm alive again\nAlive again\nOhhh, you make me feel\nLike I'm alive again\n\nSaid I can't go on, not in this way\nI'm a dream, I die by light of day\nGonna hold up half the sky and say\nOhhh, we are omen\n\nI feel my heart beating\nI feel my heart beneath my skin\nOhhh, I can feel my heart beating\nCause you make me feel\nLike I'm alive again\nAlive again...\n\nOhhh, you make me feel\nLike I'm alive again\n\nTurn your magic on, to me she'd say\nEverything you want's a dream away\nUnder this pressure, under this weight\n\nWe are diamonds taking shape!\nWe are diamonds taking shape!\n\nIf we've only got this life\nThen this adventure, more than I\nAnd if we've only got this life\nYou'll get me through alive\nAnd if we've only got this life\nThen this adventure, more than I\nWanna share with you\nWith you, with you\n\nI said, oh, say oh\n\nWoo hoo, woo hoo...",
+    var mockedResponse = new Promise(function (res) {
+        res({
+            lyrics: "Paroles de la chanson Adventure Of A Lifetime par Coldplay\r\nTurn your magic on\nTo me she'd say\nEverything you want's a dream away\nWe are legends\nEvery day\nThat's what she told him!\n\nTurn your magic on\nTo me she'd say\nEverything you want's a dream away\nUnder this pressure, under this weight\nWe are diamonds\n\nI feel my heart beating\nI feel my heart beneath my skin\nI feel my heart beating\n\n\nOhhh, you make me feel\nLike I'm alive again\nAlive again\nOhhh, you make me feel\nLike I'm alive again\n\nSaid I can't go on, not in this way\nI'm a dream, I die by light of day\nGonna hold up half the sky and say\nOhhh, we are omen\n\nI feel my heart beating\nI feel my heart beneath my skin\nOhhh, I can feel my heart beating\nCause you make me feel\nLike I'm alive again\nAlive again...\n\nOhhh, you make me feel\nLike I'm alive again\n\nTurn your magic on, to me she'd say\nEverything you want's a dream away\nUnder this pressure, under this weight\n\nWe are diamonds taking shape!\nWe are diamonds taking shape!\n\nIf we've only got this life\nThen this adventure, more than I\nAnd if we've only got this life\nYou'll get me through alive\nAnd if we've only got this life\nThen this adventure, more than I\nWanna share with you\nWith you, with you\n\nI said, oh, say oh\n\nWoo hoo, woo hoo...",
+        });
     });
 
-      var lyrics = data.lyrics.split("\r\n")[1];
-      //console.log(lyrics);
-      renderLyricsToScreen(lyrics);    
-    })
-    .catch(function (e) {
-      console.log(e);
-      modalError(e.message);
-    });
+    mockedResponse
+        .then(function (data) {
+            console.log(data);
+            //throw new Error(); removed for functionality
+            console.log(data, "data");
+
+            var lyrics = data.lyrics.split("\r\n")[1];
+            //console.log(lyrics);
+            renderLyricsToScreen(lyrics);
+        })
+        .catch(function (e) {
+            console.log(e);
+            modalError(e.message);
+        });
 }
 
 //function to display song in container
 function renderLyricsToScreen(lyrics) {
-  inputArtist.val("");
-  inputSong.val("");
-  $lyricText.text("");
-  //console.log(lyrics, "lyrics of song");
-  // console.log(individualLyric, "individual Lyrics");
-  var splitLyricsIntoLines = lyrics.split("\n");
+    inputArtist.val("");
+    inputSong.val("");
+    $lyricText.text("");
+    //console.log(lyrics, "lyrics of song");
+    // console.log(individualLyric, "individual Lyrics");
+    var splitLyricsIntoLines = lyrics.split("\n");
 
-  for (var i = 0; i < splitLyricsIntoLines.length; i++) {
-    // "" // we add a br
-    // "Text" // we wrap it in a div
-    if (lyrics[i] !== "") {
-      var paraContainer = $("<div>");
-      var words = splitLyricsIntoLines[i].split(" ");
-      for (let j = 0; j < words.length; j++) {
-        var span = $("<span>");
-        span.text(words[j]);
+    for (var i = 0; i < splitLyricsIntoLines.length; i++) {
+        // "" // we add a br
+        // "Text" // we wrap it in a div
+        if (lyrics[i] !== "") {
+            var paraContainer = $("<div>");
+            var words = splitLyricsIntoLines[i].split(" ");
+            for (let j = 0; j < words.length; j++) {
+                var span = $("<span>");
+                span.text(words[j]);
 
-        span.attr("word", words[j]);
-        paraContainer.append(span);
-        $lyricText.append(paraContainer);
-      }
-    } else {
-      var breakElem = $("<br>");
-      $lyricText.append(breakElem);
+                span.attr("word", words[j]);
+                paraContainer.append(span);
+                $lyricText.append(paraContainer);
+            }
+        } else {
+            var breakElem = $("<br>");
+            $lyricText.append(breakElem);
+        }
     }
     //Add song and Artist to recently searched - local storage.
 }
